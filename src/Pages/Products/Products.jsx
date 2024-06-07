ad6fca1fe3c73dd3fd1c3f3606f4cc2a1af71f28
habibur-01 <h.r.habibur08@gmail.com>
2024-06-07 06:14:00 +0600

    added search functionality

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -7,6 +7,9 @@ import Card from "../../Components/Shared/Card/Card";
 const Products = () => {
   const [products, setProducts] = useState([]);
   const axiosSecure = useAxiosSecure();
+  const [searchData, setSearchData] = useState();
+  const [isTrue, setIsTrue] = useState(false)
+  console.log(searchData);
 
   useEffect(() => {
     axiosSecure
@@ -19,6 +22,13 @@ const Products = () => {
       });
   }, [axiosSecure]);
   console.log(products);
+  const desiredData = products.filter(
+    (product) =>
+     ( product.name.slice(0, 3)).toLowerCase().startsWith(searchData) ||
+      product.name.includes(searchData)
+  );
+  console.log(desiredData);
+
 
   return (
     <div>
@@ -32,10 +42,12 @@ const Products = () => {
               <input
                 type="text"
                 placeholder="Search item"
+                name="data"
                 id=""
+                onChange={(e) => setSearchData(e.target.value)}
                 className="border-[1px] h-11 w-96 px-4 border-gray-300 rounded-l-3xl inline-flex"
               />
-              <button className="border-[1px] border-gray-300 h-11 px-6 rounded-r-3xl">
+              <button className="border-[1px] border-gray-300 h-11 px-6 rounded-r-3xl" onClick={()=>setIsTrue(true)}>
                 <FaSearch size={20} />
               </button>
             </div>
@@ -43,11 +55,22 @@ const Products = () => {
           <p className="text-xl font-bold">Total: {products.length}</p>
         </div>
         <div className="">
-            <h1 className="text-xl font-semibold text-gray-600 border-b-[1px] py-2 ">Show Products</h1>
+          <h1 className="text-xl font-semibold text-gray-600 border-b-[1px] py-2 ">
+            Show Products
+          </h1>
           <div className="grid lg:grid-cols-3 gap-14 justify-center mt-6 mb-20">
-            {products?.map((product) => (
+            {
+              isTrue? <>
+              {
+                desiredData?.map(product=><Card key={product._id} product={product}></Card>
+                  
+                )
+              }
+              </>:<>{products?.map((product) => (
               <Card key={product._id} product={product}></Card>
-            ))}
+            ))}</>
+            }
+           
           </div>
         </div>
       </Container>
